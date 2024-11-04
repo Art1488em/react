66c2697f09ecf8717827cf83c4bf41ffd69b9f13
Art1488em <artemdavydov401@gmail.com>
2024-11-04 19:29:11 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./App.css"
+import Hero from './components/Hero/Hero'
+import Header from './components/Header/Header'
+import Logo from './components/Logo/Logo'
+import Footer from './components/Footer/Footer'
+import Nav from './components/Nav/Nav'
+
+const App = () => {
+  return (
+    
+  <>
+  <Hero/>
+  <Header/>
+  <Logo/>
+  <Footer/>
+  <Nav/>
+  </>
+    
+  )
+}
+
+export default App
